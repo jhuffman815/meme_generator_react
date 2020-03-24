d69f1f9860db5095fb8fac5cc9e3ee5bead5b9be
Jewell <jhuffman815@gmail.com>
2020-03-24 14:56:37 -0400

    Added Random Generator Method

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -10,6 +10,7 @@ class MemeGenerator extends Component {
             allMemeImgs: []
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
     
     componentDidMount() {
@@ -26,11 +27,19 @@ class MemeGenerator extends Component {
         const {name, value} = event.target
         this.setState({ [name]: value })
     }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+        const randMemeImg = this.state.allMemeImgs[randNum].url
+
+        this.setState({ randomImg: randMemeImg })
+    }
     
     render() {
         return (
             <div>
-                <form className="meme-form">
+                <form className="meme-form" onSubmit={this.handleSubmit}>
                     <input 
                         type="text"
                         name="topText"
@@ -48,8 +57,14 @@ class MemeGenerator extends Component {
                 
                     <button>Gen</button>
                 </form>
+                <div className="meme">
+                    <img src={this.state.randomImg} alt="" />
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
+                </div>
             </div>
         )
     }
 }
+
 export default MemeGenerator
