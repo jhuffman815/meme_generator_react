5862d5242db36f35bf685598e156be2b79e604ed
Jewell <jhuffman815@gmail.com>
2020-03-24 14:35:16 -0400

    Fetched data from meme API

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,11 @@ function Header() {
     return (
         <div>
             <header>
-                <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Problem" />
+                <img 
+                    src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                    alt="Problem" />
+
+                <p>Meme Generator</p>
             </header>
         </div>
     )
